b80b7751c3807657520346d26a210e94826199c3
alexmylonas <alex.a.mylonas@gmail.com>
2015-10-20 23:23:43 +0300

    intent on display message

--- app/src/main/java/gr/alfred/omgandroid/DisplayMessageActivity.java
+++ app/src/main/java/gr/alfred/omgandroid/DisplayMessageActivity.java
@@ -24,24 +24,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        //Create the text view var
-//       TextView textView = new TextView(this);
-//        textView.setTextSize(40);
-//        textView.setText(message);
-
         ((TextView)findViewById(R.id.tvMessage)).setText(message);
-
-        // 4. get bundle from intent
-        //Bundle bundle = intent.getExtras();
-
-        // 5. get status value from bundle
-       // String status = bundle.getString("status");
-
-        // 6. show status on Toast
-  //      Toast toast = Toast.makeText(this, status, Toast.LENGTH_LONG);
-    //    toast.show();
-        //Set the text view as the activity layout
-
 
 
 
